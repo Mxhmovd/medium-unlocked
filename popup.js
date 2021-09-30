7819b9321a4338567e18ae4151d9ea5d4a95452e
Mahmoud Hamdi <32795892+Mxhmovd@users.noreply.github.com>
2021-09-30 19:31:47 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,52 @@
+  // popup script
+
+  let btn = document.querySelector(".btn")
+  console.log(btn)
+  let check = document.querySelector(".check")
+  console.log(check)
+
+  chrome.storage.local.get("check", function (result) {
+    if (result.check == null) {
+      let value = true
+      chrome.storage.local.set({
+        'check': value
+      }, function () {
+        console.log('Value is set to ' + value);
+      });
+    }
+    else {
+      console.log(result.check)
+      check.checked = result.check
+    }
+  });
+
+  check.addEventListener("change", () => {
+    if (check.checked) {
+      console.log("to")
+      value = true
+      chrome.storage.local.set({
+        'check': value
+      }, function () {
+        console.log('Value is set to ' + value);
+        check.checked = true
+      });
+    } else {
+      value = false
+      chrome.storage.local.set({
+        'check': value
+      }, function () {
+        console.log('Value is set to ' + value);
+        check.checked = false
+      });
+    }
+  })
+
+  btn.addEventListener('click', (event) => {
+    console.log("hey")
+    chrome.tabs.executeScript(null, {
+      file: 'content.js',
+    });
+    chrome.runtime.sendMessage({
+      clicked: true
+    });
+  });
